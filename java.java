56269fca8200397a3a13dca00018ad16944d6d62
pathaniamohit <132581221+pathaniamohit@users.noreply.github.com>
2023-05-17 13:26:19 -0400

    Create java.java

---
+++ java.java
@@ -0,0 +1,19 @@
+package debugger;
+
+public class debugging {
+	
+	private static int add(int int1, int int2) {
+		int sum = int1 + int2;
+		return sum;
+	}
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int firstint = 7;
+		int secondint = 7;
+		int sum = add(firstint, secondint);
+		System.out.println("sum is :" + sum);
+
+	}
+
+}
